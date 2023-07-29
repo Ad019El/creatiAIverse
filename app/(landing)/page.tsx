27e42b92d1441d4377c705109673233b016edb2f
Ad019El <a.dafi@esi-sba.dz>
2023-07-29 12:45:27 +0100

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,17 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import Image from "next/image";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-6xl text-green-400">Hello Ai SaaS Unprotected</h1>
-      <Link href="/sign-in">
-        <Button variant="secondary">Login </Button>
-      </Link>
-      <Link href="/sign-up">
-        <Button variant="secondary">Register </Button>
-      </Link>
+      <div className="h-full ">
+        <LandingNavbar />
+        <LandingHero />
+        <LandingContent />
+      </div>
     </main>
   );
 }
